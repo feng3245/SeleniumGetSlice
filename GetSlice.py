3fdaa6c034d3c0de9f1481ae7eaf0762227e38ad
Feng Liu <li123b@hotmail.com>
2018-11-02 23:12:10 -0400

    updated to be pypi format

--- GetSlice.py
+++ GetSlice.py
@@ -7,23 +7,7 @@ import sys
 import time
 from PIL import Image
 from io import BytesIO
-
-
-def save_element(driver, element, fileName):
-    location = element.location
-    size = element.size
-    png = driver.get_screenshot_as_png()
-    byteimg = BytesIO(png)
-    im = Image.open(byteimg)
-
-    left = location['x']
-    top = location['y']
-    right = location['x']+size['width']
-    bottom = location['y']+size['height']
-
-    im = im.crop((left, top, right, bottom))
-    im.save(fileName)
-
+from seleniumslicer.pageslicer import save_element
 
 
 stock = sys.argv[1]
